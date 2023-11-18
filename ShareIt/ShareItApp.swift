ccf9c74e07d985ee924d1d89e843e6f74d93c5e9
TaoufikElVU <t.el.kadi@student.vu.nl>
2023-11-18 11:58:24 +0100

    Initial Commit

---
+++ ShareIt/ShareItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareItApp.swift
+//  ShareIt
+//
+//  Created by Taoufik El Kadi on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShareItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
